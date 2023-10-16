0b4899a69b44f34ba0d76d46ec8fda699cf5ac79
Rishop Babu <rishopbabu22@gmail.com>
2023-10-16 18:14:38 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mac-OBS-51 on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
